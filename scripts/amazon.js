d46650363c68509a20fc963193d0d67ae4aff13a
Aakif <meeraakif9@gmail.com>
2025-01-08 09:18:17 +0530

    Made Add to cart button interactive - Add Starting files

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,43 +1,3 @@
-/*
-const products = 
-[{
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-        stars: 4.5,
-        count: 87
-    },
-    priceCents: 1090
-},
-{
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating: {
-        stars: 4,
-        count: 127
-    },
-    priceCents: 2095   
-},
-{
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-        stars: 4.5,
-        count: 56
-    },
-    priceCents: 799
-},
-{
-    image: 'images/products/black-2-slot-toaster.jpg',
-    name: '2 Slot Toaster - Black',
-    rating: {
-        stars: 5,
-        count: 2197
-    },
-    priceCents: 1899    
-}]
-*/
-
 let productsHTML = '';
 products.forEach((product) => {
     productsHTML += `
@@ -80,18 +40,50 @@ products.forEach((product) => {
 
             <div class="product-spacer"></div>
 
-            <div class="added-to-cart">
+            <div class="added-to-cart"> 
                 <img src="images/icons/checkmark.png">
                 Added
             </div>
 
-            <button class="add-to-cart-button button-primary">
+            <button class="add-to-cart-button button-primary js-add-to-cart"
+            data-product-id="${product.id}">
                 Add to Cart
             </button>
         </div>`;
 });
 
-console.log(productsHTML);
+//console.log(productsHTML);
 
-document.querySelector('.js-products-grid')
-    .innerHTML = productsHTML;
+// const grid = document.querySelector('.js-products-grid');
+// console.log('Found .js-products-grid:', grid);
+
+
+document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+// Now select the buttons and attach event listeners
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+    button.addEventListener('click', () => {
+        const productId = button.dataset.productId;
+
+        let matchingItem;
+
+        cart.forEach((item) => {
+            if (productId === item.productId) {
+                matchingItem = item
+            }
+        });
+
+        if (matchingItem) {
+            matchingItem.quantity += 1
+        } else {
+            cart.push({
+                productId: productId,
+                quantity: 1
+            }) 
+        }
+        console.log(cart);   
+    });
+});
+
+
+
