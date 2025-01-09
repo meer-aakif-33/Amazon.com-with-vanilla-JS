4519e0fee31a1b3af5d9daad4e956b48958b6ba9
Aakif <meeraakif9@gmail.com>
2025-01-09 23:11:10 +0530

    Updated test codes

--- tests/tests-simple/moneyTest.js
+++ tests/tests-simple/moneyTest.js
@@ -1,35 +1,27 @@
-import {formatCurrency} from "../../scripts/utils/money.js"
-
-if (formatCurrency(2005) === '20.05') {
-    console.log('passed')
-}else {
-    console.log('failed')
-}
-
-//Edge cases
-if (formatCurrency(0) === '0.00') {
-    console.log('passed')
-}else {
-    console.log('failed')
-}
-
-if (formatCurrency(2000.5) === '20.01') {
-    console.log('passed')
-}else {
-    console.log('failed')
-}
 import {formatCurrency} from '../../scripts/utils/money.js';
 
-describe('test suite: formatCurrency', () => {
-  it('converts cents into dollars', () => {
-    expect(formatCurrency(2095)).toEqual('20.95');
-  });
+console.log('test suite: formatCurrency');
 
-  it('works with 0', () => {
-    expect(formatCurrency(0)).toEqual('0.00');
-  });
+console.log('converts cents into dollars');
 
-  it('rounds up to the nearest cent', () => {
-    expect(formatCurrency(2000.5)).toEqual('20.01');
-  });
-});
+if (formatCurrency(2095) === '20.95') {
+  console.log('passed');
+} else {
+  console.log('failed');
+}
+
+console.log('works with 0');
+
+if (formatCurrency(0) === '0.00') {
+  console.log('passed');
+} else {
+  console.log('failed');
+}
+
+console.log('rounds up to the nearest cent');
+
+if (formatCurrency(2000.5) === '20.01') {
+  console.log('passed');
+} else {
+  console.log('failed');
+}
